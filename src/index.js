9075ae456320cce18b3f21e30e6b202f32fc8ec7
leadgfl13 <leadgfl13@gmail.com>
2024-02-28 21:55:22 -0500

    Create functon that makes an add project button.  Appends projects to an arrey and displays the projects in console log

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import './style.css';
 import {makeToDo} from "./project"
 import{makeProject} from "./project"
+import {testing} from "./project"
+import{display} from "./project"
+import{projectbutton} from "./project"
 
 
 
+projectbutton
 
-
-    
+testing
 const test = makeToDo('test','this is a test', 6/13, 22)
 
 let testproject = makeProject('testproject')
 
-console.log("Here is the project without adding a todo")
 
-console.log(testproject)
-console.log("now I will append using the addToDo function on testproject")
-
-testproject.addToDo('test')
-console.log(testproject)
 
 //each project is a new array, that stores the to-dos inside them??
 
 
 
 let layout = function(){
-    let body = document.getElementById('body')
+  
     let page = document.createElement('div')
     page.setAttribute('id','pagelayout')
     body.append(page)
--- src/project.js
+++ src/project.js
@@ -1,5 +1,36 @@
+// an array to hold the projects
 
-// object to make to-do's
+let projects = []
+
+let projectbutton = (function makeproj(){
+    const getProject = document.getElementById('makeproject')
+    getProject.addEventListener('click',()=>{
+        let pName = prompt("What is your project name")
+        let currentp = makeProject(pName)
+       projects.push(currentp)
+       display()
+   
+    })
+})()
+export{projectbutton}
+
+//factory function for making projects
+function makeProject(projectname){
+
+    return{
+        thing:[],
+        projectname:projectname,
+
+        addToDo(addition){
+            this.thing.push(addition)
+        }
+    }
+}
+export{makeProject}
+
+
+
+// factory function to make to-do's
 function makeToDo(title, description, duedate, priority){
     return{
         title: title,
@@ -17,32 +48,34 @@ function makeToDo(title, description, duedate, priority){
 export{makeToDo}
 
 
+// function to display the projects
+function display(){
+    for(let i =0; i<projects.length; i++){
+        console.log(projects[i])
 
-
-function makeProject(projectname){
-
-    return{
-        thing:[],
-        projectname:projectname,
-
-        addToDo(addition){
-            this.thing.push(addition)
-        }
     }
+
 }
-export{makeProject}
+export{display}
 
-function createToDo(){
-    let tdForm = document.createElement('form')
-    label
-}
 
-let addtoDO  =document.getElementById('add')
 
-addtoDO.addEventListener('click',()=>{
-    alert("Hello")
-})
+let testing = (function subtodo(){
+     const getData = document.getElementById('subtodo')
+     let name = document.getElementById('name')
+     let desc = document.getElementById('desc')
+     let date = document.getElementById('date')
+     let prio = document.getElementById('prio')
+     getData.addEventListener('click',()=>{
 
+        const thistodo = makeToDo(name.value, desc.value, date.value, prio.value )
+        projects.push(thistodo)
+        display()
+    
+     })
+})()
+
+export{testing}
 // Need a factory function for toDo
 
 // Need a function that takes Todo's and then assigns them to a project?
