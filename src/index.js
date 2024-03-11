2864ddc9973c26a9f16492f199ce916509a04eff
leadgfl13 <leadgfl13@gmail.com>
2024-03-10 21:23:46 -0400

    Hard reset

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
 import './style.css';
 import {makeToDo} from "./project"
-import{makeProject} from "./project"
-import {testing} from "./project"
-import {display} from "./project"
-import{projectbutton} from "./project"
-import { pageLayout } from './project'
-import { formappear  } from './form';
-import{makeFirst} from './project'
-import{displayToDo} from './project'
-import { renderTodo } from './project';
-import { clearToDos } from './project';
-
-
-
-
-
-
-//each project is a new array, that stores the to-dos inside them??
-
-
-makeFirst()
-pageLayout
