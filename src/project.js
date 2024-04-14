0c52f811ec17c59b4563080006d0e7a6ac0b8bbd
leadgfl13 <leadgfl13@gmail.com>
2024-04-14 10:59:19 -0400

    Add event to project buttons so that it displays todo's with the matching project name.  Clears the display every time it is hit so it doesn't display multiple times

--- src/project.js
+++ src/project.js
@@ -32,11 +32,22 @@ export class makeToDo{
 
 
 export function showTodos(projectname){
+    bottomright.innerHTML = ''
     for(let i = 0; i<todos.length; i++){
         if(todos[i].project == projectname){
+            let thistodo = todos[i]
             console.log("We have a match!")
+            makeCard(thistodo)
+            
         }
     }
+}
+
+function makeCard(atodo){
+    let card = document.createElement('div')
+    card.setAttribute('id','todocard')
+    card.innerHTML = atodo.name
+    bottomright.append(card)
 }
 
 
