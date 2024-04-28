2513758f4dda2095e4c2072012e19eb69dd535a9
leadgfl13 <leadgfl13@gmail.com>
2024-04-28 14:35:58 -0400

    Fix bug so that when deletng a project it only deletes those todos, not all todos

--- src/events.js
+++ src/events.js
@@ -18,7 +18,6 @@ function showProjects(){
 // NNED TO FIX THIS STILL!!!!!!!-----------------------------------------------------------------------------------------
     let removelist = JSON.parse(localStorage.getItem('projectlist'))
     for(let i = 0; i<projects.length; i++){
-        let currentproject = projects[i]
         let container = document.createElement('div')
         let project = document.createElement('button')
         container.append(project)
@@ -27,40 +26,42 @@ function showProjects(){
         let deleteproject = document.createElement('button')
         container.append(deleteproject)
         deleteproject.setAttribute('id', 'deleteproject')
+        project.innerHTML = projects[i].name
+        bottomleft.append(container)
+        project.addEventListener('click', ()=>{
+            showTodos(projects[i].name)
+            })
         //delete button actions
         deleteproject.addEventListener('click',()=>{
           let answer = confirm("Are you sure?")
           if(answer == true){
-          
+
             let blanktodos = JSON.parse(localStorage.getItem('todolist'))
+            console.log("Here is the todolist" + blanktodos)
+            for (let j = blanktodos.length -1; j >= 0; j--){
+                if(blanktodos[j].project==projects[i].name)
+                {
+                console.log(blanktodos[j].project + " matches " + projects[i].name)
+                todos.splice([j],1)
+                localStorage.setItem('todolist', JSON.stringify(todos))
+                }
+
+                }
+                
+                showTodos(projects[i])
             
             // deletes the project from the localstorage
-                if(currentproject == projects[i]){
+                
                     projects.splice(i,1)
                     removelist.splice(i,1)
                     localStorage.setItem('projectlist', JSON.stringify(projects))                 
                     showProjects()
-                }
+                
                 //use filter metho
-
-                for (let j = blanktodos.length -1; j >= 0; j--){
-                blanktodos.splice([j],1);
-                todos.splice(j,1)
-                localStorage.setItem('todolist', JSON.stringify(todos))
-                }
-                showTodos(currentproject)
+                
+                
                 
 
-                    //for(let j =0; (j<blanktodos.length); j++){
-
-                  //  if(currentproject.name ==blanktodos[j].project){
-                   //     blanktodos.splice(j,1)
-                   //     todos.splice(j,1)
-                   //     localStorage.setItem('todolist', JSON.stringify(todos))
-                   //     showTodos(currentproject)
-
-                 //   }
-               // }
 
             }
             else{return}
@@ -68,16 +69,6 @@ function showProjects(){
         
             )
             
-
-
-
-
-
-        project.innerHTML = currentproject.name
-        bottomleft.append(container)
-        project.addEventListener('click', ()=>{
-            showTodos(currentproject.name)
-            })
         
     }}
 export{showProjects}
