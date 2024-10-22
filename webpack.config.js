514dade95ce151062816dc0a2a2eeb71a6f8db46
leadgfl13 <leadgfl13@gmail.com>
2024-10-21 22:10:49 -0400

    Get npm working, change active button press from meme face to green color

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'docs'),
-  },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-    },
-   ],
- },
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "docs"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			title: "Template",
+			filename: "index.html",
+			inject: "head",
+			scriptloading: "defer",
+		}),
+	],
 };
